677bb2e83aa5a3314f4d00d9a6c41e758767449e
vCoelh0 <coelhocomercial2k@gmail.com>
2025-01-07 19:04:30 -0300

    Project created

---
+++ dslist/src/test/java/com/devcoelho/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcoelho.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
