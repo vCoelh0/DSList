1f94b8445c30cdb40cb1970570b702a9edd50a0a
vCoelh0 <coelhocomercial2k@gmail.com>
2025-01-07 20:07:51 -0300

    game seed

--- dslist/src/main/java/com/devcoelho/dslist/entities/Game.java
+++ dslist/src/main/java/com/devcoelho/dslist/entities/Game.java
@@ -22,8 +22,13 @@ public class Game {
 	private Integer year;
 	private String genre;
 	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	
@@ -31,13 +36,14 @@ public class Game {
 		
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String platforms, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms,Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
 		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -81,6 +87,16 @@ public class Game {
 
 	public void setPlatforms(String platforms) {
 		this.platforms = platforms;
+	}
+	
+	
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImgUrl() {
