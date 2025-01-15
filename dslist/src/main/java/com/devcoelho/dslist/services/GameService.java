50bc6c93410b0cddffa7de8c959ffeebdd0b56a9
vCoelh0 <coelhocomercial2k@gmail.com>
2025-01-14 22:01:43 -0300

    projections, busca de games por lista

--- dslist/src/main/java/com/devcoelho/dslist/services/GameService.java
+++ dslist/src/main/java/com/devcoelho/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devcoelho.dslist.dto.GameDTO;
 import com.devcoelho.dslist.dto.GameMinDTO;
 import com.devcoelho.dslist.entities.Game;
+import com.devcoelho.dslist.projections.GameMinProjection;
 import com.devcoelho.dslist.repositories.GameRepository;
 
 @Service
@@ -29,5 +30,11 @@ public class GameService {
 	return result.stream().map(x -> new GameMinDTO(x)).toList();	
 	}
 	
+	@Transactional(readOnly=true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
+	return result.stream().map(x -> new GameMinDTO(x)).toList();	
+	}
 
+	
 }
