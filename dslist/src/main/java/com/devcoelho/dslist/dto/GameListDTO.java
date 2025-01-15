61d76ddd0c2234831cd1acdfbf61bdc742618372
vCoelh0 <coelhocomercial2k@gmail.com>
2025-01-14 21:25:29 -0300

    GET lists

---
+++ dslist/src/main/java/com/devcoelho/dslist/dto/GameListDTO.java
@@ -0,0 +1,34 @@
+package com.devcoelho.dslist.dto;
+
+import com.devcoelho.dslist.entities.GameList;
+
+public class GameListDTO {
+	
+	private Long id;
+	private String name;
+	
+	
+	public GameListDTO() {
+		
+	}
+
+
+	public GameListDTO(GameList entity) {
+		id = entity.getId();
+		name = entity.getName();
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+	
+	
+	
+	
+}
